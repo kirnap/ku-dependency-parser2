433e7b3a753f3995ce312ef9f1967033d6b495f8
Omer Kirnap <okirnap@ku.edu.tr>
2018-04-07 11:47:30 +0300

    add trackers

--- src/helper.jl
+++ src/helper.jl
@@ -1,1 +1,31 @@
 # functions that are used for other purposes than machine learning or preprocessing
+
+
+macro msg(_x)
+    :(join(STDOUT,[Dates.format(now(),"HH:MM:SS"), $(esc(_x)),'\n'],' '); flush(STDOUT))
+end
+
+date(x)=(join(STDOUT,[Dates.format(now(),"HH:MM:SS"), x,'\n'],' '); flush(STDOUT))
+
+
+type StopWatch
+    tstart # start time
+    nstart # start token
+    ncurr  # current token
+    nnext  # next token
+    StopWatch()=new(time(), 0, 0, 1000)
+end
+
+
+function inc(s::StopWatch, n, step=1000)
+    s.ncurr += n
+    if s.ncurr >= s.nnext
+        tcurr = time()
+        dt = tcurr - s.tstart
+        dn = s.ncurr - s.nstart
+        s.tstart = tcurr
+        s.nstart = s.ncurr
+        s.nnext += step
+        return dn/dt
+    end
+end
