5251eaa887e6de49b8e01a669583e517b2f3101c
kirnap <okirnap@ku.edu.tr>
2018-05-29 21:04:30 +0300

    remove model file from header

--- src/header.jl
+++ src/header.jl
@@ -8,11 +8,11 @@ include("helper.jl")
 include("inits.jl")
 #include("old_features.jl")
 # Here you have seen the developments through the model
-
 #include("old_model.jl")
 #include("new_model.jl")
 #include("new_model2.jl")
-include("new_model3.jl")
+#include("new_model3.jl") # include model file in main train-file
+
 
 const MAXSENT=64       # skip longer sentences during training
 const MINSENT=2        # skip shorter sentences during training
