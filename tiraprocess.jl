60206d8187ba10b164e9ec596bb1b706e2f975d4
kirnap <okirnap@ku.edu.tr>
2018-06-15 15:05:45 +0300

    initialize tira process file

---
+++ tiraprocess.jl
@@ -0,0 +1,21 @@
+# TODO: include the final train script
+# We need to preprocess the tira files, this probably will be the final .julia file
+
+# Sample inputdir
+# "/media/training-datasets/universal-dependency-learning/conll18-ud-development-2018-05-06"
+# You may made it up your sample outdir
+function main(inputDir, outputDir)
+    metadir = joinpath(inputDir, "metadata.json")
+    metadata = JSON.parsefile(metadir)
+    for met in metadata # iterate through it
+
+        # to parse
+        outfile = joinpath(outputDir, met["outfile"]) 
+
+        # give it to erenay
+        erenayfile = joinpath(inputDir, met["psegmorfile"]) 
+
+        langcode=met["lcode"]; trcode=met["tcode"]
+        fullcode=string(langcode, "_", trcode) # give it to erenay
+    end
+end
