3061c0f33ffcb186ae8549427df8eba79825d7f1
kirnap <okirnap@ku.edu.tr>
2018-06-19 22:33:04 +0300

    wip tira process

--- tiraprocess.jl
+++ tiraprocess.jl
@@ -6,6 +6,7 @@ using JSON
 # "/media/training-datasets/universal-dependency-learning/conll18-ud-development-2018-05-06"
 # You may made it up your sample outdir
 const PARSERS = "/media/data/parser" # Put goldmodels there
+const ERENAYDIR # TODO: You need to create internal directory, where erenay creates its own file
 function main(inputDir, outputDir)
     PARSERMODELS = readdir(PARSERS)
     metadir = joinpath(inputDir, "metadata.json")
@@ -21,8 +22,10 @@ function main(inputDir, outputDir)
 
         langcode=met["lcode"]; trcode=met["tcode"]
         fullcode=string(langcode, "_", trcode) # give it to erenay
+        # example erenay code to run
 
         if string(fullcode, ".jld") in PARSERMODELS # we have a trained model
+            # inputfile = joinpath(ERENAYDIR, ) # TODO: fix this problem with erenay
             parser =  joinpath(PARSERS,  string(fullcode, ".jld"))
             println("I would look $parser")
         elseif string(langcode, ".jld") in PARSERMODELS
@@ -34,3 +37,7 @@ function main(inputDir, outputDir)
         end
     end
 end
+
+
+# This is an example code to run
+# bash srun2.sh ar_padt media/training-datasets/universal-dependency-learning/conll18-ud-trial-2018-05-06/ar_padt-udpipe.conllu /home/Kparse/mnet/myouts
