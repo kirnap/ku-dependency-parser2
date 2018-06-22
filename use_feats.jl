703e45b9898723830713027dc3eacf2c646dc875
kirnap <okirnap@ku.edu.tr>
2018-06-22 10:31:19 +0300

    meta dict

---
+++ use_feats.jl
@@ -0,0 +1,19 @@
+# Metadata containing whether to use the feats or not
+
+const use_feats = Dict{String, Bool}(
+    "ar_padt"      =>true,
+    "ru_syntagrus" =>true,
+    "cs_pdt"       =>true,
+    "en_ewt"       =>true,  # TODO:move to v3.0
+    "tr_imst"      =>false,
+    "sv_lines"     =>false,  
+    "en_gum"       =>false,
+    "ca_ancora"    =>true,
+    "hu_szeged"    =>false,
+    "ro_rrt"       =>true,
+    "sv_talbanken" =>true, 
+    "hi_hdtb"      =>true,
+    "ko_gsd"       =>false,
+    "it_isdt"      =>true,
+    "pt_bosque"    =>true,
+)
